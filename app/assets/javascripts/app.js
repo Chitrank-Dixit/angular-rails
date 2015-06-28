410057997b5de5cbf090237e958737992079d402
Chitrank-Dixit <chitrankdixit@gmail.com>
2015-06-28 11:50:09 +0530

    Initialized the angular-rails repository

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,47 @@
+angular.module('flapperNews', ['ui.router', 'templates', 'Devise'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('nav', {
+      url: '/nav',
+      templateUrl: 'nav/_nav.html',
+      controller: 'NavCtrl'
+
+    })
+    .state('home', {
+      url: '/home',
+      templateUrl: 'home/_home.html',
+      controller: 'MainCtrl'
+    })
+
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: 'posts/_posts.html',
+      controller: 'PostsCtrl'
+    })
+
+    .state('login', {
+      url: '/login',
+      templateUrl: 'auth/_login.html',
+      controller: 'AuthCtrl'
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: 'auth/_register.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'Auth', function($state, Auth) {
+        Auth.currentUser().then(function (){
+          $state.go('home');
+        })
+      }]
+    });
+
+    $urlRouterProvider.otherwise('home');
+
+  
+}]);
+
+
---
+++ app/assets/javascripts/posts/postsCtrl.js
@@ -0,0 +1,27 @@
+angular.module('flapperNews')
+.controller('PostsCtrl', [
+'$scope',
+'posts',
+'post',
+function($scope, posts, post){
+
+  $scope.post = post;
+
+  resolve: {
+    post: ['$stateParams', 'posts', function($stateParams, posts) {
+      return posts.get($stateParams.id);
+    }]
+  }
+
+  $scope.addComment = function(){
+    if($scope.body === '') { return; }
+    posts.addComment(post.id, {
+      body: $scope.body,
+      author: 'user',
+    }).success(function(comment) {
+      $scope.post.comments.push(comment);
+    });
+    $scope.body = '';
+  };
+
+}]);
